333106da280ac11ce23f1dfa73b4233fef364ea0
Joaotp75 <114034705+Joaotp75@users.noreply.github.com>
2024-08-21 16:15:17 -0300

    Add files via upload

--- dashboard_lojas.py
+++ dashboard_lojas.py
@@ -9,7 +9,6 @@ import pandas as pd
 import plotly.express as px
 
 # Carregar os dados das planilhas "Lojas" e "Cidades"
-
 df_lojas = pd.read_excel('Case 2 - Base de Dados.xlsx', sheet_name='Lojas')
 df_cidades = pd.read_excel('Case 2 - Base de Dados.xlsx', sheet_name='Cidades')
 
@@ -37,45 +36,45 @@ with tab1:
         df_filtrado = df_cidades[df_cidades['CIDADES'].isin(cidades_selecionadas)]
 
         st.write("### Receita Bruta por Cidade")
-        receita_por_ano_cidade = df_filtrado[[col for col in df_cidades.columns if 'Receita Bruta' in col]]
-        st.dataframe(receita_por_ano_cidade.T.rename_axis("Ano", axis=1).rename_axis("Cidade", axis=1), use_container_width=True)
-        fig_receita_cidade = px.bar(receita_por_ano_cidade.T, title='Receita Bruta por Cidade', barmode='group')
+        receita_por_ano_cidade = df_filtrado[['CIDADES'] + [col for col in df_cidades.columns if 'Receita Bruta' in col]].set_index('CIDADES').T
+        st.dataframe(receita_por_ano_cidade.rename_axis("Ano", axis=0), use_container_width=True)
+        fig_receita_cidade = px.bar(receita_por_ano_cidade, title='Receita Bruta por Cidade', barmode='group')
         st.plotly_chart(fig_receita_cidade, use_container_width=True)
 
         st.write("### EBITDA Bruto por Cidade")
-        ebitda_por_ano_cidade = df_filtrado[[col for col in df_cidades.columns if 'EBITDA' in col and not 'habitante' in col and not 'm2' in col and not 'Margem' in col]]
-        st.dataframe(ebitda_por_ano_cidade.T.rename_axis("Ano", axis=1).rename_axis("Cidade", axis=1), use_container_width=True)
-        fig_ebitda_cidade = px.bar(ebitda_por_ano_cidade.T, title='EBITDA Bruto por Cidade', barmode='group')
+        ebitda_por_ano_cidade = df_filtrado[['CIDADES'] + [col for col in df_cidades.columns if 'EBITDA' in col and not 'habitante' in col and not 'm2' in col and not 'Margem' in col]].set_index('CIDADES').T
+        st.dataframe(ebitda_por_ano_cidade.rename_axis("Ano", axis=0), use_container_width=True)
+        fig_ebitda_cidade = px.bar(ebitda_por_ano_cidade, title='EBITDA Bruto por Cidade', barmode='group')
         st.plotly_chart(fig_ebitda_cidade, use_container_width=True)
 
         st.write("### Margem EBITDA por Cidade")
-        margem_ebitda_cidade = df_filtrado[[col for col in df_cidades.columns if 'Margem EBITDA' in col]]
-        st.dataframe(margem_ebitda_cidade.T.rename_axis("Ano", axis=1).rename_axis("Cidade", axis=1), use_container_width=True)
-        fig_margem_ebitda_cidade = px.bar(margem_ebitda_cidade.T, title='Margem EBITDA por Cidade', barmode='group')
+        margem_ebitda_cidade = df_filtrado[['CIDADES'] + [col for col in df_cidades.columns if 'Margem EBITDA' in col]].set_index('CIDADES').T
+        st.dataframe(margem_ebitda_cidade.rename_axis("Ano", axis=0), use_container_width=True)
+        fig_margem_ebitda_cidade = px.bar(margem_ebitda_cidade, title='Margem EBITDA por Cidade', barmode='group')
         st.plotly_chart(fig_margem_ebitda_cidade, use_container_width=True)
 
         st.write("### Receita/habitante por Cidade")
-        receita_hab_cidade = df_filtrado[[col for col in df_cidades.columns if 'Receita/habitante' in col]]
-        st.dataframe(receita_hab_cidade.T.rename_axis("Ano", axis=1).rename_axis("Cidade", axis=1), use_container_width=True)
-        fig_receita_hab_cidade = px.bar(receita_hab_cidade.T, title='Receita/habitante por Cidade', barmode='group')
+        receita_hab_cidade = df_filtrado[['CIDADES'] + [col for col in df_cidades.columns if 'Receita/habitante' in col]].set_index('CIDADES').T
+        st.dataframe(receita_hab_cidade.rename_axis("Ano", axis=0), use_container_width=True)
+        fig_receita_hab_cidade = px.bar(receita_hab_cidade, title='Receita/habitante por Cidade', barmode='group')
         st.plotly_chart(fig_receita_hab_cidade, use_container_width=True)
 
         st.write("### EBITDA/habitante por Cidade")
-        ebitda_hab_cidade = df_filtrado[[col for col in df_cidades.columns if 'EBITDA/habitante' in col]]
-        st.dataframe(ebitda_hab_cidade.T.rename_axis("Ano", axis=1).rename_axis("Cidade", axis=1), use_container_width=True)
-        fig_ebitda_hab_cidade = px.bar(ebitda_hab_cidade.T, title='EBITDA/habitante por Cidade', barmode='group')
+        ebitda_hab_cidade = df_filtrado[['CIDADES'] + [col for col in df_cidades.columns if 'EBITDA/habitante' in col]].set_index('CIDADES').T
+        st.dataframe(ebitda_hab_cidade.rename_axis("Ano", axis=0), use_container_width=True)
+        fig_ebitda_hab_cidade = px.bar(ebitda_hab_cidade, title='EBITDA/habitante por Cidade', barmode='group')
         st.plotly_chart(fig_ebitda_hab_cidade, use_container_width=True)
 
         st.write("### Receita/m² por Cidade")
-        receita_m2_cidade = df_filtrado[[col for col in df_cidades.columns if 'Receita/m2' in col]]
-        st.dataframe(receita_m2_cidade.T.rename_axis("Ano", axis=1).rename_axis("Cidade", axis=1), use_container_width=True)
-        fig_receita_m2_cidade = px.bar(receita_m2_cidade.T, title='Receita/m² por Cidade', barmode='group')
+        receita_m2_cidade = df_filtrado[['CIDADES'] + [col for col in df_cidades.columns if 'Receita/m2' in col]].set_index('CIDADES').T
+        st.dataframe(receita_m2_cidade.rename_axis("Ano", axis=0), use_container_width=True)
+        fig_receita_m2_cidade = px.bar(receita_m2_cidade, title='Receita/m² por Cidade', barmode='group')
         st.plotly_chart(fig_receita_m2_cidade, use_container_width=True)
 
         st.write("### EBITDA/m² por Cidade")
-        ebitda_m2_cidade = df_filtrado[[col for col in df_cidades.columns if 'EBITDA/m2' in col]]
-        st.dataframe(ebitda_m2_cidade.T.rename_axis("Ano", axis=1).rename_axis("Cidade", axis=1), use_container_width=True)
-        fig_ebitda_m2_cidade = px.bar(ebitda_m2_cidade.T, title='EBITDA/m² por Cidade', barmode='group')
+        ebitda_m2_cidade = df_filtrado[['CIDADES'] + [col for col in df_cidades.columns if 'EBITDA/m2' in col]].set_index('CIDADES').T
+        st.dataframe(ebitda_m2_cidade.rename_axis("Ano", axis=0), use_container_width=True)
+        fig_ebitda_m2_cidade = px.bar(ebitda_m2_cidade, title='EBITDA/m² por Cidade', barmode='group')
         st.plotly_chart(fig_ebitda_m2_cidade, use_container_width=True)
 
     # Visualização por loja
@@ -85,46 +84,47 @@ with tab1:
         df_filtrado = df_lojas[df_lojas['Loja'].isin(lojas_selecionadas)]
 
         st.write("### Receita Bruta por Loja")
-        receita_bruta_por_ano = df_filtrado[[col for col in df_lojas.columns if 'Receita Bruta' in col]].T
-        st.dataframe(receita_bruta_por_ano.rename_axis("Ano", axis=0).rename_axis("Cidade", axis=1), use_container_width=True)
+        receita_bruta_por_ano = df_filtrado[['Loja'] + [col for col in df_lojas.columns if 'Receita Bruta' in col]].set_index('Loja').T
+        st.dataframe(receita_bruta_por_ano.rename_axis("Ano", axis=0), use_container_width=True)
         fig_receita_loja = px.bar(receita_bruta_por_ano, title='Receita Bruta por Loja', barmode='group')
         st.plotly_chart(fig_receita_loja, use_container_width=True)
 
         st.write("### EBITDA Bruto por Loja")
-        ebitda_por_ano = df_filtrado[[col for col in df_lojas.columns if 'EBITDA' in col and not 'habitante' in col and not 'm2' in col and not 'Margem' in col]].T
-        st.dataframe(ebitda_por_ano.rename_axis("Ano", axis=0).rename_axis("Cidade", axis=1), use_container_width=True)
+        ebitda_por_ano = df_filtrado[['Loja'] + [col for col in df_lojas.columns if 'EBITDA' in col and not 'habitante' in col and not 'm2' in col and not 'Margem' in col]].set_index('Loja').T
+        st.dataframe(ebitda_por_ano.rename_axis("Ano", axis=0), use_container_width=True)
         fig_ebitda_loja = px.bar(ebitda_por_ano, title='EBITDA Bruto por Loja', barmode='group')
         st.plotly_chart(fig_ebitda_loja, use_container_width=True)
 
         st.write("### Margem EBITDA por Loja")
-        margem_ebitda_loja = df_filtrado[[col for col in df_lojas.columns if 'Margem EBITDA' in col]].T
-        st.dataframe(margem_ebitda_loja.rename_axis("Ano", axis=0).rename_axis("Cidade", axis=1), use_container_width=True)
+        margem_ebitda_loja = df_filtrado[['Loja'] + [col for col in df_lojas.columns if 'Margem EBITDA' in col]].set_index('Loja').T
+        st.dataframe(margem_ebitda_loja.rename_axis("Ano", axis=0), use_container_width=True)
         fig_margem_ebitda_loja = px.bar(margem_ebitda_loja, title='Margem EBITDA por Loja', barmode='group')
         st.plotly_chart(fig_margem_ebitda_loja, use_container_width=True)
 
         st.write("### Receita/habitante por Loja")
-        receita_hab_loja = df_filtrado[[col for col in df_lojas.columns if 'Receita/habitante' in col]].T
-        st.dataframe(receita_hab_loja.rename_axis("Ano", axis=0).rename_axis("Cidade", axis=1), use_container_width=True)
+        receita_hab_loja = df_filtrado[['Loja'] + [col for col in df_lojas.columns if 'Receita/habitante' in col]].set_index('Loja').T
+        st.dataframe(receita_hab_loja.rename_axis("Ano", axis=0), use_container_width=True)
         fig_receita_hab_loja = px.bar(receita_hab_loja, title='Receita/habitante por Loja', barmode='group')
         st.plotly_chart(fig_receita_hab_loja, use_container_width=True)
 
         st.write("### EBITDA/habitante por Loja")
-        ebitda_hab_loja = df_filtrado[[col for col in df_lojas.columns if 'EBITDA/habitante' in col]].T
-        st.dataframe(ebitda_hab_loja.rename_axis("Ano", axis=0).rename_axis("Cidade", axis=1), use_container_width=True)
+        ebitda_hab_loja = df_filtrado[['Loja'] + [col for col in df_lojas.columns if 'EBITDA/habitante' in col]].set_index('Loja').T
+        st.dataframe(ebitda_hab_loja.rename_axis("Ano", axis=0), use_container_width=True)
         fig_ebitda_hab_loja = px.bar(ebitda_hab_loja, title='EBITDA/habitante por Loja', barmode='group')
         st.plotly_chart(fig_ebitda_hab_loja, use_container_width=True)
 
         st.write("### Receita/m² por Loja")
-        receita_m2_loja = df_filtrado[[col for col in df_lojas.columns if 'Receita/m2' in col]].T
-        st.dataframe(receita_m2_loja.rename_axis("Ano", axis=0).rename_axis("Cidade", axis=1), use_container_width=True)
+        receita_m2_loja = df_filtrado[['Loja'] + [col for col in df_lojas.columns if 'Receita/m2' in col]].set_index('Loja').T
+        st.dataframe(receita_m2_loja.rename_axis("Ano", axis=0), use_container_width=True)
         fig_receita_m2_loja = px.bar(receita_m2_loja, title='Receita/m² por Loja', barmode='group')
         st.plotly_chart(fig_receita_m2_loja, use_container_width=True)
 
         st.write("### EBITDA/m² por Loja")
-        ebitda_m2_loja = df_filtrado[[col for col in df_lojas.columns if 'EBITDA/m2' in col]].T
-        st.dataframe(ebitda_m2_loja.rename_axis("Ano", axis=0).rename_axis("Cidade", axis=1), use_container_width=True)
+        ebitda_m2_loja = df_filtrado[['Loja'] + [col for col in df_lojas.columns if 'EBITDA/m2' in col]].set_index('Loja').T
+        st.dataframe(ebitda_m2_loja.rename_axis("Ano", axis=0), use_container_width=True)
         fig_ebitda_m2_loja = px.bar(ebitda_m2_loja, title='EBITDA/m² por Loja', barmode='group')
         st.plotly_chart(fig_ebitda_m2_loja, use_container_width=True)
+
 with tab2:
     st.header("Ordenação por Desempenho")
     
@@ -156,5 +156,5 @@ with tab2:
     df_sorted = df_lojas if "Loja" in metric else df_cidades
     df_sorted = df_sorted.sort_values(by=[metric], ascending=(ordem == "Crescente"))
     st.write(f"Ranking das lojas/cidades baseado em {metric}:")
-    st.dataframe(df_sorted[['Loja', metric]] if "Loja" in metric else df_sorted[['CIDADES', metric]])
+    st.dataframe(df_sorted[['Loja', 'Cidade', metric]] if "Loja" in metric else df_sorted[['CIDADES', metric]])
 
